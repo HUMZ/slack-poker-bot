22d14aa5b46993959e673b908f064d0e7075533f
Humz <hnasir@gmail.com>
2019-03-24 21:27:37 -0400

    working on AI

--- src/bot.js
+++ src/bot.js
@@ -131,13 +131,13 @@ class Bot {
   startGame(messages, channel, players) {
     if (players.length <= 1) {
       channel.send('Not enough human players.  Adding an AI player to join you. Type @poker quit to end game.');
-      addBotPlayers(players) {
+      
         // let bot1 = new WeakBot('Bee Bot');
         // players.push(bot1);
          
          let bot2 = new AggroBot('Bo Bot');
          players.push(bot2);
-       }
+       
     }
 
     channel.send(`We've got ${players.length} players, let's start the game.`);
@@ -171,7 +171,7 @@ class Bot {
   // players - The players participating in the game
 
   
-
+  addBotPlayers(players) {}
 
   // Private: Save which channels and groups this bot is in and log them.
   onClientOpened() {
